81f06aadde5f0e9522ef189a5984bb3ddb35bf2e
tncrdk <thnchrdjk@gmail.com>
2023-08-07 23:41:18 +0200

    oppdatert feil-typer

---
+++ src/error.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from result import Err
+from dataclasses import dataclass
+
+
+class ErrorLevel(Enum):
+    ERROR = 0
+    WARNING = 1
+
+
+@dataclass
+class BaseError:
+    msg: str
+    level: ErrorLevel = ErrorLevel.ERROR
+
+
+class FormatError(BaseError):
+    pass
+
+
+class DatabaseError(BaseError):
+    pass
+
+
+def create_error(msg: str) -> Err[BaseError]:
+    return Err(BaseError(msg))
+
+def create_warning(msg: str) -> Err[BaseError]:
+    return Err(BaseError(msg, ErrorLevel.WARNING))
