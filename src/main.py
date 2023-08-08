55377182f1de0d881bec77846b91216d8c49d445
tncrdk <thnchrdjk@gmail.com>
2023-08-08 23:07:19 +0200

    lagt til databasen

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 import discord
 import os
-from .bot import run_bot
+from bot import run_bot
+from replit import db
+
 
 if __name__ == "__main__":
     intents = discord.Intents.default()
     intents.message_content = True
     intents.members = True
     client = discord.Client(intents=intents)
-    # server = client.guilds[0]
     TOKEN = os.getenv("token")
 
     if TOKEN is None:
         raise KeyError("Fant ikke TOKEN i env-variablene")
 
-    run_bot(client, TOKEN)
+    if db is None:
+        raise Exception("Fant ikke databasen")
+
+    run_bot(client, TOKEN, db)
