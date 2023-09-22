35fce6175af9aadcd711486bed6839efabdfb2cf
tncrdk <thnchrdjk@gmail.com>
2023-09-22 23:26:55 +0200

    fikset cmd-line argumenter

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from database import Database
 from pathlib import Path
 
 
-
 def main(debug: bool):
     intents = discord.Intents.default()
     intents.message_content = True
@@ -20,23 +19,24 @@ def main(debug: bool):
         raise KeyError("Fant ikke TOKEN i env-variablene")
 
     if debug:
-        SAVE_DIR = Path(__file__).parent
+        save_dir = Path(__file__).parent
     else:
-        SAVE_DIR = Path("/persistent_database")
-    DATABASE_PATH = SAVE_DIR / ".database.pkl" 
-    ID_PATH = SAVE_DIR / ".ID"
+        save_dir = Path("/persistent_database")
+    database_path = save_dir / ".database.pkl"
+    id_path = save_dir / ".ID"
 
-    database = Database(DATABASE_PATH, ID_PATH)
+    database = Database(database_path, id_path)
 
     run_bot(client, TOKEN, database)
 
+
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) < 2 or args[1] == "debug":
-        debug=True
-    elif args[1] == "release":
+    if len(args) <= 1 or args[1] in ("debug", "dev"):
+        debug = True
+    elif args[1] in ("release", "prod"):
         debug = False
     else:
         print(f"{args[1:]} is not a valid argument")
-        debug = True
+        sys.exit()
     main(debug)
