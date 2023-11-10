11bee91416efc1c17e6646ed851dbc1f9a2b6553
tncrdk <thnchrdjk@gmail.com>
2023-11-10 22:00:29 +0100

    formatting

--- src/channels.py
+++ src/channels.py
@@ -5,8 +5,6 @@ general_handler = message_handler.GeneralHandler()
 CHANNELS = [message_handler.QuotesHandler(), general_handler]
 
 
-
-
 def get_botchannel_by_ID(ID: int) -> Optional[message_handler.MessageHandler]:
     for channel in CHANNELS:
         if channel.ID == ID:
